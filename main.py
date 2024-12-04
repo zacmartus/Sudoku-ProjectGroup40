c58798836bde73c34b9046fd0fe246b4d6855c11
lukas <lukasspamzzzz@gmail.com>
2024-12-03 20:07:08 -0500

    changed number_font

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sudoku")
 number_font = pygame.font.Font(None, NUMBER_FONT)
 
+board = SudokuGenerator(9, 30)
+board.fill_diagonal()
+board.fill_remaining(0, 0)
+board.remove_cells()
+updated = board.get_board()
+
+game_over = False
+wins = 0
+
 def draw_grid():
     # draw horizontal lines
     for i in range(1, BOARD_ROWS):
@@ -66,9 +75,47 @@ def draw_grid():
 
 
 def draw_numbers():
-    number_surf = number_font.render('1', True, LINE_COLOR)
-    number_rect = number_surf.get_rect(center=(270, 270))
-    screen.blit(number_surf, number_rect)
+    number_surf1 = number_font.render('1', True, LINE_COLOR)
+    number_surf2 = number_font.render('2', True, LINE_COLOR)
+    number_surf3 = number_font.render('3', True, LINE_COLOR)
+    number_surf4 = number_font.render('4', True, LINE_COLOR)
+    number_surf5 = number_font.render('5', True, LINE_COLOR)
+    number_surf6 = number_font.render('6', True, LINE_COLOR)
+    number_surf7 = number_font.render('7', True, LINE_COLOR)
+    number_surf8 = number_font.render('8', True, LINE_COLOR)
+    number_surf9 = number_font.render('9', True, LINE_COLOR)
+
+    number_rect = number_surf1.get_rect(center=(270, 270))
+
+    for row in range(0, 9):
+        for col in range(0, 9):
+            if updated[row][col] == '1':
+                number_rect = number_surf1.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf1, number_rect)
+            elif updated[row][col] == '2':
+                number_rect = number_surf2.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf2, number_rect)
+            elif updated[row][col] == '3':
+                number_rect = number_surf3.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf3, number_rect)
+            elif updated[row][col] == '4':
+                number_rect = number_surf4.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf4, number_rect)
+            elif updated[row][col] == '5':
+                number_rect = number_surf5.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf5, number_rect)
+            elif updated[row][col] == '6':
+                number_rect = number_surf6.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf6, number_rect)
+            elif updated[row][col] == '7':
+                number_rect = number_surf7.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf7, number_rect)
+            elif updated[row][col] == '8':
+                number_rect = number_surf8.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf8, number_rect)
+            elif updated[row][col] == '9':
+                number_rect = number_surf9.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                screen.blit(number_surf9, number_rect)
 
 
 screen.fill(BG_COLOR)
@@ -80,5 +127,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            row = y // SQUARE_SIZE
+            col = x // SQUARE_SIZE
 
     pygame.display.update()
